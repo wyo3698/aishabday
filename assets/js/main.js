c7ba96f89195df0476ed5b37cfc7a2b8d2b04e16
wyandrew3698 <wyandrew@gmail.com>
2024-10-26 09:57:17 +0800

    Updated css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,38 +33,7 @@
 	// 	alignment: 'center',
 	// 	detach: false
 	// });
-
-	// Nav.
-
-	// Title Bar.
-	$(
-		'<div id="titleBar">' +
-		'<a href="#navPanel" class="toggle"></a>' +
-		'<span class="title">' + $('#logo h1').html() + '</span>' +
-		'</div>'
-	)
-		.appendTo($body);
-
-	// Panel.
-	$(
-		'<div id="navPanel">' +
-		'<nav>' +
-		$('#nav').navList() +
-		'</nav>' +
-		'</div>'
-	)
-		.appendTo($body)
-		.panel({
-			delay: 500,
-			hideOnClick: true,
-			hideOnSwipe: true,
-			resetScroll: true,
-			resetForms: true,
-			side: 'left',
-			target: $body,
-			visibleClass: 'navPanel-visible'
-		});
-
+	
 	//New
 	// script.js
 	// script.js
